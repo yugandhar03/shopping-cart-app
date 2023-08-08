cb4b37890aeb088a7d4160df6c6a24cd423edc5c
Yugandhar <yugandhar@arokee.com>
2023-08-08 19:54:25 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import ProductCatalog from './pages/product/ProductCatalog';
+import ShoppingCart from './pages/cart/ShoppingCart';
+import CheckoutSuccess from './pages/Payment/CheckoutSuccess';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      Shopping cart appliction
-    </div>
+
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductCatalog />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/checkout-success" element={<CheckoutSuccess />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ const bodyParser =require('body-parser');
 const mongoose =require('mongoose');
 const cors =require('cors');
 const dotenv =require('dotenv');
-
+const apiRoutes = require('./routes/apiRoutes');
 dotenv.config();
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use(apiRoutes);
+
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
